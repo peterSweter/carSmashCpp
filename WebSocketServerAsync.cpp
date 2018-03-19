5325b2a6794899d68376e900a71969379b55b81b
Piotr Uchwat <peteruchwat@gmail.com>
2018-03-19 17:22:05 +0100

    Added web socket serwer.

---
+++ WebSocketServerAsync.cpp
@@ -0,0 +1,5 @@
+//
+// Created by peter on 2/10/18.
+//
+
+#include "WebSocketServerAsync.h"
---
+++ WebSocketServerAsync.h
@@ -0,0 +1,20 @@
+//
+// Created by peter on 2/10/18.
+//
+
+#ifndef CARSMASHCPP_WEBSOCKETSERVERASYNC_H
+#define CARSMASHCPP_WEBSOCKETSERVERASYNC_H
+
+
+#include <vector>
+
+class WebSocketServerAsync {
+
+public:
+
+    std::vector<int> getClients();
+
+};
+
+
+#endif //CARSMASHCPP_WEBSOCKETSERVERASYNC_H
